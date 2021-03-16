9fa714431384e182a79d87d3b418d5cc88e64700
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-03-15 23:34:12 -0300

    1st part of exercise

---
+++ bloco_6/script.js
@@ -0,0 +1,47 @@
+const dropdown = document.querySelector('#input-state'); // gets select tag
+// console.log(dropdown);
+
+let states = {
+    'AC': 'Acre',
+    'AL': 'Alagoas',
+    'AP': 'Amapá',
+    'AM': 'Amazonas',
+    'BA': 'Bahia',
+    'CE': 'Ceará',
+    'DF': 'Distrito Federal',
+    'ES': 'Espírito Santo',
+    'GO': 'Goiás',
+    'MA': 'Maranhão',
+    'MT': 'Mato Grosso',
+    'MS': 'Mato Grosso do Sul',
+    'MG': 'Minas Gerais',
+    'PA': 'Pará',
+    'PB': 'Paraíba',
+    'PR': 'Paraná',
+    'PE': 'Pernambuco',
+    'PI': 'Piauí',
+    'RJ': 'Rio de Janeiro',
+    'RN': 'Rio Grande do Norte',
+    'RS': 'Rio Grande do Sul',
+    'RO': 'Rondônia',
+    'RR': 'Roraima',
+    'SC': 'Santa Catarina',
+    'SP': 'São Paulo',
+    'SE': 'Sergipe',
+    'TO': 'Tocantins'
+};
+
+// put the State's names in the select tag
+function putStates() {
+    for (const key in states) {
+        let option = document.createElement('option');
+        option.value = key;
+        option.innerText = states[key];
+        if (key == 'DF') {
+            option.setAttribute('selected', '');
+        }
+        dropdown.appendChild(option);
+    }
+}
+
+putStates();
