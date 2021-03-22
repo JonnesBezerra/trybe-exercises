bf7affd18aacebef82099ae326ca650c9c9a4777
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-03-21 21:19:06 -0300

    Exercise 2

--- bloco_4/dia_2/practice.js
+++ bloco_4/dia_2/practice.js
@@ -3,5 +3,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
+    console.log(`index ${i}: ${numbers[i]}`);
 }
+
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+console.log(`Sum = ${sum}`);
