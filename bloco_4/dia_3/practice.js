2e822ed036b2803d4d4269eda8f4e9ad774615a9
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-03-22 13:27:29 -0300

    Exercise 2 -> 4.3

--- bloco_4/dia_3/practice.js
+++ bloco_4/dia_3/practice.js
@@ -1,7 +1,6 @@
 // Pratice exercise 4.3
 
 let n = 5;
-let v = [];
 
 if (n > 1) {
     for (let i = 0; i < n; i++) {
@@ -11,3 +10,17 @@ if (n > 1) {
         console.log();
     }
 }
+
+let manyAsterisks = 1;
+
+if (n > 1) {
+    for (let i = 0; i < n; i++) {
+        for (let i = 0; i < manyAsterisks; i++) {
+            process.stdout.write('*');
+        }
+        manyAsterisks++;
+        console.log();
+    }
+}
+
+let 
