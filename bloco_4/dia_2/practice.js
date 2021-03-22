25eea800c8229229763cad706beddc32dbedc1cb
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-03-21 21:33:08 -0300

    Exercise 4

--- bloco_4/dia_2/practice.js
+++ bloco_4/dia_2/practice.js
@@ -14,3 +14,17 @@ console.log(`Sum = ${sum}`);
 
 let average = (sum / numbers.length);
 console.log(`Average: ${average}`);
+
+if (average > 20) {
+    console.log('valor maior que 20');
+} else {
+    console.log('valor menor ou igual a 20');
+}
+
+let bigger = numbers[0];
+for (let i = 0; i < numbers.length; i++) {
+    if (bigger < numbers[i]) {
+        bigger = numbers[i];
+    }
+}
+console.log(bigger);
